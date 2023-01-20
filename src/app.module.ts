1b838b8794190be0e4a9eb6f56d8dfe9f41e7e6d
moutasem <bumoutasem@gmail.com>
2023-01-20 15:12:56 +0200

    [VOID-21][add]:prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppDatabaseModule } from '@app/app-database/app-database.module';
 import { AppConfigModule } from '@app/app-config/app-config.module';
 import { SharedModule } from '@app/modules/shared.module';
 import { AppDefaultController } from '@app/app-controllers/app-default.controller';
+import { AppPrismaModule } from './app-prisma/app-prisma.module';
 
 @Module({
-  imports: [AppConfigModule, SharedModule, AppGraphqlModule, AppDatabaseModule],
+  imports: [AppConfigModule, SharedModule, AppGraphqlModule, AppDatabaseModule, AppPrismaModule],
   controllers: [AppDefaultController],
-  providers: [],
 })
 export class AppModule {}
