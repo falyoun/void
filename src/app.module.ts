3d4875968c6c03372ce04b869cf9b1f8e8486f6b
moutasem <bumoutasem@gmail.com>
2023-01-28 12:59:52 +0200

    [VOID-21][update]: review nots

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppGraphqlModule } from '@app/app-grpahql/app-graphql.module';
 import { AppConfigModule } from '@app/app-config/app-config.module';
-import { SharedModule } from '@app/modules/shared.module';
 import { AppDefaultController } from '@app/app-controllers/app-default.controller';
 import { AppPrismaModule } from '@app/app-prisma/app-prisma.module';
+import { RegistrationModule } from '@app/modules/registration/registration.module';
 
 @Module({
-  imports: [AppConfigModule, SharedModule, AppGraphqlModule, AppPrismaModule],
+  imports: [AppConfigModule, RegistrationModule, AppGraphqlModule, AppPrismaModule],
   controllers: [AppDefaultController],
 })
 export class AppModule {}
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersResolver } from '@app/modules/users/resolvers/users.resolver';
 import { UsersService } from '@app/modules/users/services/users.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { User } from '@app/modules/users/models/user.model';
 import { AppPrismaModule } from '@app/app-prisma/app-prisma.module';
 
 @Module({
